af31536c2a0ba98a38a26e92eed6eb0a086ed716
kevin-ko <kevin.alex.ko@gmail.com>
2024-06-24 00:40:44 +0800

    .

--- app/page.jsx
+++ app/page.jsx
@@ -460,24 +460,24 @@ const QuickNav = () => {
 //   );
 // };
 //////////////////////////////////
-const sectionAnimateObserver = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("active");
-      } else {
-        entry.target.classList.remove("active");
-      }
-    });
-  },
-  { threshold: 0.2 }
-);
+// const sectionAnimateObserver = new IntersectionObserver(
+//   (entries) => {
+//     entries.forEach((entry) => {
+//       if (entry.isIntersecting) {
+//         entry.target.classList.add("active");
+//       } else {
+//         entry.target.classList.remove("active");
+//       }
+//     });
+//   },
+//   { threshold: 0.2 }
+// );
 
-/////////////////////////////////////////////
-const sectionAnimateElements = document.querySelectorAll(".section-animate");
-sectionAnimateElements.forEach((element) => {
-  sectionAnimateObserver.observe(element);
-});
+// /////////////////////////////////////////////
+// const sectionAnimateElements = document.querySelectorAll(".section-animate");
+// sectionAnimateElements.forEach((element) => {
+//   sectionAnimateObserver.observe(element);
+// });
 
 //
 const Tools = () => {
