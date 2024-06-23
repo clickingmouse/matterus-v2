4daaa0c82dee700db9d14593b46233547476c239
kevin-ko <kevin.alex.ko@gmail.com>
2024-06-23 15:01:33 +0800

    placeholder text on landing

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center">
+      <h1 className=" head_text text-center">discore & share</h1>
+      <br className="max-md:hidden" />
+      <span className="orange_gradient">IOT</span>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eveniet
+        repudiandae, fugit nemo velit quaerat sit odio nam voluptas in maxime
+        mollitia laboriosam totam laudantium, natus ut vitae officiis. Facilis?
+      </p>
+    </section>
+  );
 };
 
 export default Home;
