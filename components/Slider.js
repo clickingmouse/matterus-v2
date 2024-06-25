244a4a75536262faf5b592cbc74dc2bef5b8fe3c
kevin-ko <kevin.alex.ko@gmail.com>
2024-06-25 11:50:53 +0800

    start integration slider

---
+++ components/Slider.js
@@ -0,0 +1,56 @@
+<div class="slider-container">
+  <h1>
+    {" "}
+    MatterUs 智能家居方案 <i class="fa-solid fa-house-circle-check"></i>
+  </h1>
+  <div class="swiper">
+    <div class="swiper-wrapper">
+      <div class="swiper-slide">
+        <img src="https://i.imgur.com/LBivabb.jpeg" />
+        <div class="slide-overlay">
+          <h2>智能共享方案</h2>
+          <button>
+            方案詳情及收費 <i class="fa-solid fa-person-breastfeeding"></i>
+          </button>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <img src="https://i.imgur.com/IFsyKpe.jpeg" />
+        <div class="slide-overlay">
+          <h2>智能掌控方案</h2>
+          <button>
+            方案詳情及收費 <i class="fa-solid fa-tower-cell"></i>
+          </button>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <img src="https://i.imgur.com/2vTBlqv.jpeg" />
+        <div class="slide-overlay">
+          <h2>有聲有色方案</h2>
+          <button>
+            方案詳情及收費 <i class="fa-brands fa-steam"></i>
+          </button>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <img src="https://i.imgur.com/BUIvsiM.jpeg" />
+        <div class="slide-overlay">
+          <h2>慳錢慳電方案</h2>
+          <button>
+            方案詳情及收費 <i class="fa-solid fa-seedling"></i>
+          </button>
+        </div>
+      </div>
+      <div class="swiper-slide">
+        <img src="https://i.imgur.com/aXtqJTg.png" />
+        <div class="slide-overlay">
+          <h2>一條龍服務🐉</h2>
+          <button>
+            預約免費諮詢 <i class="fa-solid fa-fire"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</div>;
